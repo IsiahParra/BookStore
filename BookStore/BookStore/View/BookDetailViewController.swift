0916027314d4501d7d4d7e987d045a765768e051
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-05-12 15:52:45 -0600

    Updating work for outlets and actions

---
+++ BookStore/BookStore/View/BookDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BookDetailViewController.swift
+//  BookStore
+//
+//  Created by Isiah Parra on 5/12/22.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    @IBOutlet weak var tellUsAboutThisBook: UILabel!
+    @IBOutlet weak var bookPhoto: UIImageView!
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var bookAuthorTextField: UITextField!
+    @IBOutlet weak var bookRatingTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    @IBAction func clearButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
